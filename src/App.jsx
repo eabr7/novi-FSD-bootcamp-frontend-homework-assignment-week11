efe4df6efd6dea1e6043f448945e8286b456d682
eabr7 <e.bruins@protonmail.com>
2025-12-04 10:36:54 +0100

    Step 1a

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import './App.css'
 
 function App() {
   return (
+      <>
       <h1>Handbags & Purses</h1>
+
+
+      <nav>
+          <button className="/" type="button" onClick={() => console.log("to the collection")}>to the collection</button>
+          <button className="/" type="button" onClick={() => console.log("shop all bags")}>shop all bags</button>
+          <button className="/" type="button" disabled={true}>pre-orders</button>
+      </nav>
+
+
+      </>
 
   )
 }
